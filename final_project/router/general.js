7608ba9c6f1e05fcad2f5248fcb2bcb66078efeb
keinve91 <kevincrzz.14.05@gmail.com>
2025-12-07 01:01:27 -0300

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,15 +17,11 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  // 1. Recuperamos el ISBN de los parámetros de la URL
   const isbn = req.params.isbn;
   
-  // 2. Buscamos en nuestra base de datos local
   if (books[isbn]) {
-      // Si existe, lo devolvemos
       return res.status(200).json(books[isbn]);
   } else {
-      // Si no existe, devolvemos un error 404
       return res.status(404).json({message: "Libro no encontrado"});
   }
  });
@@ -34,19 +30,16 @@ public_users.get('/isbn/:isbn', function (req, res) {
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   
-  // 1. Obtenemos todas las claves del objeto 'books' (1, 2, 3...)
   const bookKeys = Object.keys(books);
   
   const booksByAuthor = [];
 
-  // 2. Iteramos a través de las claves y verificamos si el autor coincide
   bookKeys.forEach(key => {
     if (books[key].author === author) {
       booksByAuthor.push(books[key]);
     }
   });
 
-  // 3. Devolvemos la lista de libros encontrados
   if (booksByAuthor.length > 0) {
     return res.status(200).json(booksByAuthor);
   } else {
@@ -55,9 +48,24 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  
+  const bookKeys = Object.keys(books);
+  
+  const booksByTitle = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].title === title) {
+      booksByTitle.push(books[key]);
+    }
+  });
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({message: "Título no encontrado"});
+  }
 });
 
 //  Get book review
